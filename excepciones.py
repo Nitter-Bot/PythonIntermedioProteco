c570ad8d5afbe79a5bcb90abc836f0eb6ef77cc4
Nitter <samiher89@gmail.com>
2024-01-21 21:59:13 -0600

    Excepciones created
    Creacion del archivo excepciones para el proyecto

---
+++ excepciones.py
@@ -0,0 +1,14 @@
+#Curso python intermedio    Samuel Hernandez Chavez
+#Programa que calculara el imc usando excepciones
+
+def datos():
+    peso = float(input("Ingresa tu peso en kg : "))
+    altura = float(input("Ingresa tu altura en metros: "))
+
+    return peso,altura
+
+x,y = datos()
+
+print(x)
+print(y)
+
