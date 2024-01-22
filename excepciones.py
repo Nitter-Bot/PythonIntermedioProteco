23f11122c3a0347484a89d572eafe712efa04de7
Nitter <samiher89@gmail.com>
2024-01-21 22:09:04 -0600

    Excepciones creado
    Crear archivo py del ejercicio

--- excepciones.py
+++ excepciones.py
@@ -2,13 +2,33 @@
 #Programa que calculara el imc usando excepciones
 
 def datos():
-    peso = float(input("Ingresa tu peso en kg : "))
-    altura = float(input("Ingresa tu altura en metros: "))
+    peso,altura = 0,0
+    while peso<=0:
+        try:
+            peso = float(input("\nIngresa tu peso en kg : "))
+            if peso<=0:
+                print("Ingresa solo numeros positivos mayores a 0")
+        except:
+            print("Ingresa solo numeros")
+    while altura<=0 or altura>=3:
+        try:
+
+            altura = float(input("\nIngresa tu altura en metros: "))
+            if altura<=0:
+                print("\aIngresa numeros positivos mayores a 0")
+            elif altura>=0:
+                print("\aPorfavor nadie mide mas de 3 metros")
+
+        except:
+            print("\aSolo ingresa un numero en metros")
 
     return peso,altura
 
-x,y = datos()
 
-print(x)
-print(y)
+try:
+    x,y = datos()
 
+    print(x)
+    print(y)
+except:
+    print("Solo ingresa numeros")
