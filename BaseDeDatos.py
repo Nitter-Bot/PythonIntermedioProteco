6885a91adf3e712ba658b95ad2442e3599edaaba
Nitter <samiher89@gmail.com>
2024-01-26 17:58:21 -0600

    Base de datos
    Creacion del ejercicio para la base de datpos

---
+++ BaseDeDatos.py
@@ -0,0 +1,18 @@
+"""
+Crear una base de datos y las funciones correspondientes
+para crear, actualizar, eliminar y actualizar.
+"""
+
+#Empezamos importando la libreria correspondiente
+import sqlite3
+
+
+def menu():
+    print'[1] Crear nueva tabla'
+    print'[2] Generar registros'
+    print'[3] Leer tabla'
+    print'[4] Actualizar resgistros'
+    print'[5] Borrar registros'
+
+print('Bienvenido')
+
